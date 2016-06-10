8c66b67ec10a7e96c7eb221e1aa2b326ef636340
Coty Beasley <coty@cotybeasley.name>
2016-06-10 13:11:05 -0500

    Added sass

--- filter.js
+++ filter.js
@@ -1,31 +1,19 @@
 // go to https://designers.mx/mixes?page=50
 
 $(window).bind("load", function() {
-    var soundcloud = 'this_has_soundcloud';
-    var spotify = 'this_has_spotify';
-    var rdio = 'this_has_rdio';
-
-    var IDAlbums = function() {
-
-        /* Adds Classes To All Album Parents */
-        $('.indicator--rdio').closest('.mix--preview').addClass(rdio);
-        $('.indicator--soundcloud').closest('.mix--preview').addClass(soundcloud);
-        $('.indicator--spotify').closest('.mix--preview').addClass(spotify);
-    };
 
     /* Removes All But Spotify */
     var removeAllBut = function(service) {
+        $('.indicator--' + service).closest('.mix--preview').addClass('this_has_' + service);
         // $('.mix--preview').not('.' + service).remove();
-        $('.mix--preview').not('.' + service).hide();
     };
 
-    $('.mixes').prepend('<div id="xyz"></div>');
+    $('body').delay(4000).addClass('active').append('<div id="xyz"></div>');
     $('.mixes__navigation').append('<div id="xyz"></div>');
     $('#xyz').load(chrome.extension.getURL("content.html"), function() {
         $('#testButtonYo').on('click', function() {
-            IDAlbums();
-            removeAllBut(rdio);
-            console('You clicked dat button, yo!');
+            removeAllBut('spotify');
+            console.log('You clicked dat button, yo!');
         });
     });
 });
