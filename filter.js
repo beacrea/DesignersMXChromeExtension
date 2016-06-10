22b1a63055029d9000e53512d34ad20f6411c73e
Coty Beasley <coty@cotybeasley.name>
2016-06-10 14:03:51 -0500

    Highlighting works and basic functions are solid

--- filter.js
+++ filter.js
@@ -2,18 +2,32 @@
 
 $(window).bind("load", function() {
 
-    /* Removes All But Spotify */
-    var removeAllBut = function(service) {
-        $('.indicator--' + service).closest('.mix--preview').addClass('this_has_' + service);
-        // $('.mix--preview').not('.' + service).remove();
+    /* Reset Styling */
+    var clearFilter = function() {
+        $('.mix--preview, .mix--preview > a').removeClass('this_has_rdio this_has_soundcloud this_has_spotify').css('opacity', '1').css('border', '0');
     };
 
-    $('body').delay(4000).addClass('active').append('<div id="xyz"></div>');
+    /* Removes All But Chosen */
+    var removeAllBut = function(service) {
+        $('.indicator--' + service).closest('.mix--preview').addClass('this_has_' + service);
+        $('.mix--preview').not('.this_has_' + service).css('opacity', '0.1');
+        $('.this_has_' + service + '> a').css('border', '1px solid white');
+    };
+
+    $('html').delay(4000).addClass('active').append('<div id="xyz"></div>');
     $('.mixes__navigation').append('<div id="xyz"></div>');
     $('#xyz').load(chrome.extension.getURL("content.html"), function() {
-        $('#testButtonYo').on('click', function() {
+        $('#btn_spotify').on('click', function() {
+            clearFilter();
             removeAllBut('spotify');
-            console.log('You clicked dat button, yo!');
+        });
+        $('#btn_rdio').on('click', function() {
+            clearFilter();
+            removeAllBut('rdio');
+        });
+        $('#btn_soundcloud').on('click', function() {
+            clearFilter();
+            removeAllBut('soundcloud');
         });
     });
 });
