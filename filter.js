2d5010c8f1cb125c2867e1749403522ee28512e4
Coty Beasley <coty@cotybeasley.name>
2016-06-10 18:33:34 -0500

    Added text color to filter links

--- filter.js
+++ filter.js
@@ -5,6 +5,7 @@ $(window).bind("load", function() {
     /* Reset Styling */
     var clearFilter = function() {
         $('.mix--preview, .mix--preview > a').removeClass('this_has_rdio this_has_soundcloud this_has_spotify').css('opacity', '1').css('border', '0');
+        $('#xyz button').removeClass('active');
     };
 
     /* Removes All But Chosen */
@@ -19,14 +20,17 @@ $(window).bind("load", function() {
         $('#btn_spotify').on('click', function() {
             clearFilter();
             removeAllBut('spotify', '#8bc305');
+            $(this).addClass('active');
         });
         $('#btn_rdio').on('click', function() {
             clearFilter();
             removeAllBut('rdio', '#2d90ce');
+            $(this).addClass('active');
         });
         $('#btn_soundcloud').on('click', function() {
             clearFilter();
             removeAllBut('soundcloud', '#dc4303');
+            $(this).addClass('active');
         });
         $('#btn_reset').on('click', function() {
             clearFilter();
